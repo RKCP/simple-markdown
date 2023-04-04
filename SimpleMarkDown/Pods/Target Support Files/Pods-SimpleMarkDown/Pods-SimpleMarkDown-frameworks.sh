49eb6c4066a6f2e1965f72f37b0c237e1f611f00
RAPHAEL PETERS <RKCPETERS@gmail.com>
2023-04-04 23:34:07 +0100

    Add CoreStore pod to project

--- SimpleMarkDown/Pods/Target Support Files/Pods-SimpleMarkDown/Pods-SimpleMarkDown-frameworks.sh
+++ SimpleMarkDown/Pods/Target Support Files/Pods-SimpleMarkDown/Pods-SimpleMarkDown-frameworks.sh
@@ -177,10 +177,12 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CLTypingLabel/CLTypingLabel.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CoreStore/CoreStore.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/IQKeyboardManagerSwift/IQKeyboardManagerSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CLTypingLabel/CLTypingLabel.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CoreStore/CoreStore.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/IQKeyboardManagerSwift/IQKeyboardManagerSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
